07bce71d256490eb8a49970e8f8630c6810ae393
MAR�A STELLA IDARRAGA RAMIREZ <mariaidarraga1163747@correo.itm.edu.co>
2026-03-05 00:31:03 -0500

    Add Trapeze

---
+++ Figure_Geometric/Figure_Geometric.Backend/Trapeze.cs
@@ -0,0 +1,43 @@
+﻿namespace Figure_Geometric.Backend;
+
+public class Trapeze : Triangle
+{
+    // Field
+    private double _d;
+
+    // Property
+    public double D
+    {
+        get { return _d; }
+        set
+        {
+            ValidateD(value);
+            _d = value;
+        }
+    }
+
+
+    public Trapeze(string name, double a, double b, double c, double d, double h)
+        : base(name, a, b, c, h)
+    {
+        D = d;
+    }
+
+    public override double GetArea()
+    {
+        return (B + D) * H / 2;
+    }
+
+    public override double GetPerimeter()
+    {
+        return A + B + C + D;
+    }
+
+    private void ValidateD(double value)
+    {
+        if (value <= 0)
+        {
+            throw new ArgumentException("D must be greater than zero");
+        }
+    }
+}
