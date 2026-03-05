c4011e88a691c6bd2d9887f90a080259367751bb
MAR�A STELLA IDARRAGA RAMIREZ <mariaidarraga1163747@correo.itm.edu.co>
2026-03-05 00:25:50 -0500

    Add Parallelogram

---
+++ Figure_Geometric/Figure_Geometric.Backend/Parallelogram.cs
@@ -0,0 +1,43 @@
+﻿namespace Figure_Geometric.Backend;
+
+public class Parallelogram : Rectangle
+{
+    // Field
+    private double _h;
+
+    // Property
+    public double H
+    {
+        get { return _h; }
+        set
+        {
+            ValidateH(value);
+            _h = value;
+        }
+    }
+
+
+    public Parallelogram(string name, double a, double b, double h)
+        : base(name, a, b)
+    {
+        H = h;
+    }
+
+    public override double GetArea()
+    {
+        return B * H;
+    }
+
+    public override double GetPerimeter()
+    {
+        return 2 * (A + B);
+    }
+
+    private void ValidateH(double value)
+    {
+        if (value <= 0)
+        {
+            throw new ArgumentException("H must be greater than zero");
+        }
+    }
+}
