8cb0c4e67010035c097aed040030d8917b193a79
MAR�A STELLA IDARRAGA RAMIREZ <mariaidarraga1163747@correo.itm.edu.co>
2026-03-05 14:38:55 -0500

    Multiplicacion

---
+++ Figure_Geometric/Rhombus/MatrixMultiplication/Program.cs
@@ -0,0 +1,74 @@
+﻿using Shared;
+
+Console.Write("Ingrese el valor de m: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese el valor de n: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese el valor de p: ");
+int p = int.Parse(Console.ReadLine());
+
+int[,] A = new int[m, n];
+int[,] B = new int[n, p];
+int[,] C = new int[m, p];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        A[i, j] = (i + 1) * j;
+    }
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < p; j++)
+    {
+        B[i, j] = (j + 1) * i;
+    }
+}
+
+Console.WriteLine("*** A ***");
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(A[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("*** B ***");
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < p; j++)
+    {
+        Console.Write(B[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < p; j++)
+    {
+        for (int k = 0; k < n; k++)
+        {
+            C[i, j] += A[i, k] * B[k, j];
+        }
+    }
+}
+
+Console.WriteLine("*** C ***");
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < p; j++)
+    {
+        Console.Write(C[i, j] + " ");
+    }
+    Console.WriteLine();
+}
