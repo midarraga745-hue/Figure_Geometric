d3b39def1ff943bb93c4a09f20972b9caddb2683
MAR�A STELLA IDARRAGA RAMIREZ <mariaidarraga1163747@correo.itm.edu.co>
2026-03-05 00:28:37 -0500

    Add Triangle

---
+++ Figure_Geometric/Figure_Geometric.Backend/Triangle.cs
@@ -0,0 +1,70 @@
+﻿
+
+namespace Figure_Geometric.Backend;
+
+public class Triangle : Rectangle
+{
+    // Fields
+    private double _c;
+    private double _h;
+
+    // Properties
+    public double C
+    {
+        get { return _c; }
+        set
+        {
+            ValidateC(value);
+            _c = value;
+        }
+    }
+
+    public double H
+    {
+        get { return _h; }
+        set
+        {
+            ValidateH(value);
+            _h = value;
+        }
+    }
+
+    // Methods
+    public Triangle(string name, double a, double b, double c, double h) : base(name, a, b)
+    {
+        Name = name;
+        C = c;
+        H = h;
+
+    }
+
+
+    public override double GetArea()
+    {
+        return (B * H) / 2;
+    }
+
+
+    public override double GetPerimeter()
+    {
+
+        return A + B + C;
+    }
+
+    private void ValidateC(double value)
+    {
+        if (value <= 0)
+        {
+            throw new ArgumentException("C must be greater than zero");
+        }
+    }
+
+
+    private void ValidateH(double value)
+    {
+        if (value <= 0)
+        {
+            throw new ArgumentException("H must be greater than zero");
+        }
+    }
+}
