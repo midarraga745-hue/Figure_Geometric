ea5428ecf8d75d0a64efd8b7bcc63057a515f967
MAR�A STELLA IDARRAGA RAMIREZ <mariaidarraga1163747@correo.itm.edu.co>
2026-03-05 00:04:29 -0500

    Add Kite

---
+++ Figure_Geometric/Figure_Geometric.Backend/Kite.cs
@@ -0,0 +1,44 @@
+﻿namespace Figure_Geometric.Backend;
+
+public class Kite : Rhombus
+{
+
+    // Field
+    private double _b;
+
+    // Property
+    public double B
+    {
+        get { return _b; }
+        set
+        {
+            ValidateB(value);
+            _b = value;
+        }
+    }
+
+    // Constructor correcto llamando al base
+    public Kite(string name, double a, double d1, double d2, double b)
+        : base(name, a, d1, d2)
+    {
+        B = b;
+    }
+
+    public override double GetArea()
+    {
+        return (D1 * D2) / 2;
+    }
+
+    public override double GetPerimeter()
+    {
+        return 2 * (A + B);
+    }
+
+    private void ValidateB(double value)
+    {
+        if (value <= 0)
+        {
+            throw new ArgumentException("The value of B must be greater than zero");
+        }
+    }
+}
