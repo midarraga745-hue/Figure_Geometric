07fb2ed53b49fa1407653f4491dc619c43bd4ea0
MAR�A STELLA IDARRAGA RAMIREZ <mariaidarraga1163747@correo.itm.edu.co>
2026-03-06 09:57:16 -0500

    Add Rhombus

---
+++ Figure_Geometric/AppliedLogic/Applied_Logic/Logic_Applied/Rhombus/Program.cs
@@ -0,0 +1,51 @@
+﻿using Shared;
+
+var answer = "s";
+var options = new List<string> { "s", "n" };
+
+while (answer == "s")
+{
+
+    int n = ConsoleExtension.GetInt("Enter another rhombus: ");
+
+
+    for (int i = 1; i <= n; i += 2)
+    {
+        int spaces = (n - i) / 2;
+
+        for (int j = 0; j < spaces; j++)
+            Console.Write(" ");
+
+        for (int j = 0; j < i; j++)
+        {
+            if (j == 0 || j == i - 1)
+                Console.Write("#");
+            else
+                Console.Write(" ");
+        }
+
+        Console.WriteLine();
+    }
+
+
+    for (int i = n - 2; i >= 1; i -= 2)
+    {
+        int spaces = (n - i) / 2;
+
+        for (int j = 0; j < spaces; j++)
+            Console.Write(" ");
+
+        for (int j = 0; j < i; j++)
+        {
+            if (j == 0 || j == i - 1)
+                Console.Write("#");
+            else
+                Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+
+    answer = ConsoleExtension.GetValidOptions("¿You wish to enter another rhombus? (s/n): ",
+        options
+        ) ?? "n";
+}
